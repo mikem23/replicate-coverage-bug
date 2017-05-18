c8c983e3f9bfb2f1e3ae2c2bee9786b75af0fa5f
Mike McLean <mikem@redhat.com>
2017-05-18 13:21:04 -0400

    run coverage directly

--- run.sh
+++ run.sh
@@ -3,4 +3,6 @@
 # NOTE: sometimes takes more than one try to hit the bug
 
 coverage erase
-PYTHONPATH=. python3  /usr/bin/nosetests-3 -v --with-coverage
+export PYTHONPATH=.
+#python3  /usr/bin/nosetests-3 -v --with-coverage
+coverage3 run /usr/bin/nosetests-3 -v
